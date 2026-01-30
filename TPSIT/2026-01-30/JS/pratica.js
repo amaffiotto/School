bf83bc30717515429c9dc55a73ccad544c884c54
Andrea <a.maffiotto.4046@vallauri.edu>
2026-01-30 11:56:19 +0100

    Lezione ripasso

---
+++ TPSIT/2026-01-30/JS/pratica.js
@@ -0,0 +1,7 @@
+let nome = 'Luca';
+let msg = `ciao ${nome}`;
+console.log(msg);
+console.log(nome.length);
+console.log(nome.charAt(0));
+console.log(nome.indexOf('L'));
+console.log(nome.substring(1, 2));
