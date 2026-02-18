f88937f5564c513c66dff8d1f6e5f70b00db6c66
Andrea Maffiotto <a.maffiotto.4046@vallauri.edu>
2026-02-18 12:29:16 +0100

    es22 finito

--- Informatica/Maffiotto - es22p116/Form1.Designer.cs
+++ Informatica/Maffiotto - es22p116/Form1.Designer.cs
@@ -28,21 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lstCantanti = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.lstCanzoni = new System.Windows.Forms.ListBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.lstPunteggi = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstHitparade = new System.Windows.Forms.ListBox();
+            this.btnCerca = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lstCantanti
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 43);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 264);
-            this.listBox1.TabIndex = 0;
+            this.lstCantanti.FormattingEnabled = true;
+            this.lstCantanti.Location = new System.Drawing.Point(12, 43);
+            this.lstCantanti.Name = "lstCantanti";
+            this.lstCantanti.Size = new System.Drawing.Size(120, 264);
+            this.lstCantanti.TabIndex = 0;
             // 
             // label1
             // 
@@ -62,13 +65,13 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "label2";
             // 
-            // listBox2
+            // lstCanzoni
             // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(138, 43);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(120, 264);
-            this.listBox2.TabIndex = 2;
+            this.lstCanzoni.FormattingEnabled = true;
+            this.lstCanzoni.Location = new System.Drawing.Point(138, 43);
+            this.lstCanzoni.Name = "lstCanzoni";
+            this.lstCanzoni.Size = new System.Drawing.Size(120, 264);
+            this.lstCanzoni.TabIndex = 2;
             // 
             // label3
             // 
@@ -79,25 +82,55 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "label3";
             // 
-            // listBox3
+            // lstPunteggi
             // 
-            this.listBox3.FormattingEnabled = true;
-            this.listBox3.Location = new System.Drawing.Point(264, 43);
-            this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(120, 264);
-            this.listBox3.TabIndex = 4;
+            this.lstPunteggi.FormattingEnabled = true;
+            this.lstPunteggi.Location = new System.Drawing.Point(264, 43);
+            this.lstPunteggi.Name = "lstPunteggi";
+            this.lstPunteggi.Size = new System.Drawing.Size(120, 264);
+            this.lstPunteggi.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(390, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // lstHitparade
+            // 
+            this.lstHitparade.FormattingEnabled = true;
+            this.lstHitparade.Location = new System.Drawing.Point(390, 43);
+            this.lstHitparade.Name = "lstHitparade";
+            this.lstHitparade.Size = new System.Drawing.Size(237, 264);
+            this.lstHitparade.TabIndex = 6;
+            // 
+            // btnCerca
+            // 
+            this.btnCerca.Location = new System.Drawing.Point(12, 314);
+            this.btnCerca.Name = "btnCerca";
+            this.btnCerca.Size = new System.Drawing.Size(615, 49);
+            this.btnCerca.TabIndex = 8;
+            this.btnCerca.Text = "Riempi Vettori e calcola hit parade";
+            this.btnCerca.UseVisualStyleBackColor = true;
+            this.btnCerca.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerca);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lstHitparade);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.lstPunteggi);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.lstCanzoni);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lstCantanti);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -107,12 +140,15 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lstCantanti;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox lstCanzoni;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox lstPunteggi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstHitparade;
+        private System.Windows.Forms.Button btnCerca;
     }
 }
 
--- Informatica/Maffiotto - es22p116/Form1.cs
+++ Informatica/Maffiotto - es22p116/Form1.cs
@@ -12,9 +12,91 @@ namespace Maffiotto___es22p116
 {
     public partial class Form1 : Form
     {
+        string[] cantanti = new string[30]
+{
+    "Alessandra Amoroso", "Angelina Mango", "Annalisa", "Annalisa", "Elodie",
+    "Emma", "Geolier", "Geolier", "Giorgia", "Irama",
+    "Jay-Z", "Jay-Z", "Kanye West", "Kanye West", "Lazza",
+    "Lazza", "Ligabue", "Madame", "Marco Mengoni", "Pinguini Tattici",
+    "Pinguini Tattici", "Playboi Carti", "Playboi Carti", "Sfera Ebbasta", "Tananai",
+    "Tedua", "The Kolors", "Ultimo", "Vasco Rossi", "Zucchero"
+};
+
+        string[] canzoni = new string[30]
+        {
+    "Fino a qui", "La Noia", "Sinceramente", "Euforia", "Tribale",
+    "Apnea", "I p' me, tu p' te", "L'ultima poesia", "Come Saprei", "Tu no",
+    "Empire State of Mind", "The Story of O.J.", "Stronger", "Runaway", "Cenere",
+    "100 Messaggi", "Certe Notti", "Marea", "Due Vite", "Pastello Bianco",
+    "Ricordi", "Magnolia", "Sky", "Visiera a becco", "Veleno",
+    "Red Light", "Un ragazzo una ragazza", "Piccola Stella", "Albachiara", "Baila"
+        };
+
+        int[] punteggi = new int[30];
+        Random rnd = new Random();
+        void BubbleSortEClassifica(int[] voti, string[] nomi, string[] brani)
+        {
+            int n = voti.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (voti[j] < voti[j + 1])
+                    {
+                        int tempVoto = voti[j];
+                        voti[j] = voti[j + 1];
+                        voti[j + 1] = tempVoto;
+
+                        string tempNome = nomi[j];
+                        nomi[j] = nomi[j + 1];
+                        nomi[j + 1] = tempNome;
+
+                        string tempBrano = brani[j];
+                        brani[j] = brani[j + 1];
+                        brani[j + 1] = tempBrano;
+                    }
+                }
+            }
+
+            int[] posizioni = new int[n];
+            int rango = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i > 0 && voti[i] < voti[i - 1])
+                {
+                    rango++;
+                }
+                posizioni[i] = rango;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                voti[i] = posizioni[i];
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+            lstCantanti.Items.AddRange(cantanti);
+            lstCanzoni.Items.AddRange(canzoni);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lstPunteggi.Items.Clear();
+            lstHitparade.Items.Clear();
+            for (int i = 0; i < punteggi.Length; i++)
+            {
+                punteggi[i] = rnd.Next(1, 51);
+                lstPunteggi.Items.Add(punteggi[i]);
+            }
+            BubbleSortEClassifica(punteggi, cantanti, canzoni);
+            for (int i = punteggi.Length - 1;i >= 0;i--)
+            {
+                lstHitparade.Items.Add($"{punteggi[i]} - {canzoni[i]} - {cantanti[i]}");
+            }
         }
     }
 }
