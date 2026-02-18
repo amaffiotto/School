b28a44c040ea4d238b959bbab358ffdbbae24e65
Andrea Maffiotto <a.maffiotto.4046@vallauri.edu>
2026-02-18 11:07:22 +0100

    es18

---
+++ Informatica/Maffiotto - es18p116/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Maffiotto___es18p116
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCerca = new System.Windows.Forms.Button();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lstCanzoni = new System.Windows.Forms.ListBox();
+            this.lstCantanti = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCerca
+            // 
+            this.btnCerca.Location = new System.Drawing.Point(335, 192);
+            this.btnCerca.Name = "btnCerca";
+            this.btnCerca.Size = new System.Drawing.Size(75, 23);
+            this.btnCerca.TabIndex = 0;
+            this.btnCerca.Text = "Cerca";
+            this.btnCerca.UseVisualStyleBackColor = true;
+            this.btnCerca.Click += new System.EventHandler(this.btnCerca_Click);
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(335, 222);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(120, 95);
+            this.lstOutput.TabIndex = 1;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(335, 166);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 2;
+            // 
+            // lstCanzoni
+            // 
+            this.lstCanzoni.FormattingEnabled = true;
+            this.lstCanzoni.Location = new System.Drawing.Point(133, 119);
+            this.lstCanzoni.Name = "lstCanzoni";
+            this.lstCanzoni.Size = new System.Drawing.Size(120, 95);
+            this.lstCanzoni.TabIndex = 3;
+            // 
+            // lstCantanti
+            // 
+            this.lstCantanti.FormattingEnabled = true;
+            this.lstCantanti.Location = new System.Drawing.Point(133, 276);
+            this.lstCantanti.Name = "lstCantanti";
+            this.lstCantanti.Size = new System.Drawing.Size(120, 95);
+            this.lstCantanti.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Lista Canzoni";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Lista Cantanti";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Inserire nome del cantante";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstCantanti);
+            this.Controls.Add(this.lstCanzoni);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.btnCerca);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCerca;
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ListBox lstCanzoni;
+        private System.Windows.Forms.ListBox lstCantanti;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Informatica/Maffiotto - es18p116/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maffiotto___es18p116
+{
+    public partial class Form1 : Form
+    {
+        string[] cantanti = new string[15]
+{
+    "Alessandra Amoroso",
+    "Angelina Mango",
+    "Annalisa",
+    "Elodie",
+    "Emma",
+    "Geolier",
+    "Giorgia",
+    "Irama",
+    "Lazza",
+    "Ligabue",
+    "Madame",
+    "Marco Mengoni",
+    "Pinguini Tattici",
+    "Ultimo",
+    "Vasco Rossi"
+};
+
+        string[] canzoni = new string[15]
+        {
+    "Fino a qui",
+    "La Noia",
+    "Sinceramente",
+    "Tribale",
+    "Apnea",
+    "I p' me, tu p' te",
+    "Come Saprei",
+    "Tu no",
+    "Cenere",
+    "Certe Notti",
+    "Marea",
+    "Due Vite",
+    "Pastello Bianco",
+    "Piccola Stella",
+    "Albachiara"
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            lstCantanti.Items.AddRange(cantanti);
+            lstCanzoni.Items.AddRange(canzoni);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCerca_Click(object sender, EventArgs e)
+        {
+            string target = txtInput.Text ?? "";
+            bool superato = false;
+            int i = 0;
+            lstOutput.Items.Clear();
+            if (target.Length > 3)
+            {
+                while (!superato && i < cantanti.Length)
+                {
+                    if (target.CompareTo(cantanti[i]) < 0)
+                    {
+                        superato = true;
+                    }
+                    if (target == cantanti[i])
+                    {
+                            lstOutput.Items.Add(canzoni[i]);
+                    }
+                    i++;
+                }
+            }
+        }
+    }
+}
