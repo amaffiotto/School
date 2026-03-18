fc00fb29c142de24637507d92c175856f2aca511
Andrea <a.maffiotto.4046@vallauri.edu>
2026-03-18 11:45:03 +0100

    Verifica

---
+++ Informatica/[VER] 2026-3-18/MaffiottoAndrea/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace MaffiottoAndrea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstStudenti = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstVoti = new System.Windows.Forms.ListBox();
+            this.lblStudenti = new System.Windows.Forms.Label();
+            this.lblVoti = new System.Windows.Forms.Label();
+            this.lblRis = new System.Windows.Forms.Label();
+            this.lstRisultato = new System.Windows.Forms.ListBox();
+            this.txtNinput = new System.Windows.Forms.TextBox();
+            this.txtCinput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstStudenti
+            // 
+            this.lstStudenti.FormattingEnabled = true;
+            this.lstStudenti.Location = new System.Drawing.Point(46, 48);
+            this.lstStudenti.Name = "lstStudenti";
+            this.lstStudenti.Size = new System.Drawing.Size(120, 238);
+            this.lstStudenti.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1046, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Genera voti calcola media e visualizza risultato";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstVoti
+            // 
+            this.lstVoti.FormattingEnabled = true;
+            this.lstVoti.Location = new System.Drawing.Point(172, 48);
+            this.lstVoti.Name = "lstVoti";
+            this.lstVoti.Size = new System.Drawing.Size(120, 238);
+            this.lstVoti.TabIndex = 2;
+            // 
+            // lblStudenti
+            // 
+            this.lblStudenti.AutoSize = true;
+            this.lblStudenti.Location = new System.Drawing.Point(46, 29);
+            this.lblStudenti.Name = "lblStudenti";
+            this.lblStudenti.Size = new System.Drawing.Size(49, 13);
+            this.lblStudenti.TabIndex = 3;
+            this.lblStudenti.Text = "Studenti:";
+            this.lblStudenti.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblVoti
+            // 
+            this.lblVoti.AutoSize = true;
+            this.lblVoti.Location = new System.Drawing.Point(172, 28);
+            this.lblVoti.Name = "lblVoti";
+            this.lblVoti.Size = new System.Drawing.Size(28, 13);
+            this.lblVoti.TabIndex = 4;
+            this.lblVoti.Text = "Voti:";
+            // 
+            // lblRis
+            // 
+            this.lblRis.AutoSize = true;
+            this.lblRis.Location = new System.Drawing.Point(298, 28);
+            this.lblRis.Name = "lblRis";
+            this.lblRis.Size = new System.Drawing.Size(51, 13);
+            this.lblRis.TabIndex = 6;
+            this.lblRis.Text = "Risultato:";
+            // 
+            // lstRisultato
+            // 
+            this.lstRisultato.FormattingEnabled = true;
+            this.lstRisultato.Location = new System.Drawing.Point(298, 48);
+            this.lstRisultato.Name = "lstRisultato";
+            this.lstRisultato.Size = new System.Drawing.Size(794, 238);
+            this.lstRisultato.TabIndex = 5;
+            // 
+            // txtNinput
+            // 
+            this.txtNinput.Location = new System.Drawing.Point(1098, 64);
+            this.txtNinput.Name = "txtNinput";
+            this.txtNinput.Size = new System.Drawing.Size(100, 20);
+            this.txtNinput.TabIndex = 7;
+            // 
+            // txtCinput
+            // 
+            this.txtCinput.Location = new System.Drawing.Point(1097, 108);
+            this.txtCinput.Name = "txtCinput";
+            this.txtCinput.Size = new System.Drawing.Size(100, 20);
+            this.txtCinput.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1098, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Inserire N (max 20)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1097, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Inserire C:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1363, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCinput);
+            this.Controls.Add(this.txtNinput);
+            this.Controls.Add(this.lblRis);
+            this.Controls.Add(this.lstRisultato);
+            this.Controls.Add(this.lblVoti);
+            this.Controls.Add(this.lblStudenti);
+            this.Controls.Add(this.lstVoti);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstStudenti);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstStudenti;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstVoti;
+        private System.Windows.Forms.Label lblStudenti;
+        private System.Windows.Forms.Label lblVoti;
+        private System.Windows.Forms.Label lblRis;
+        private System.Windows.Forms.ListBox lstRisultato;
+        private System.Windows.Forms.TextBox txtNinput;
+        private System.Windows.Forms.TextBox txtCinput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Informatica/[VER] 2026-3-18/MaffiottoAndrea/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaffiottoAndrea
+{
+    public partial class Form1 : Form
+    {
+        string[] congomi =
+        {
+            "Aigotti",
+            "Bianchi",
+            "Conti",
+            "Druzzo",
+            "Erti",
+            "Findus",
+            "Giaccardi",
+            "Hilton",
+            "Isaia",
+            "Jhon",
+            "Kleda",
+            "Leon",
+            "Maffiotto",
+            "Nadir",
+            "Ometto",
+            "Perrone",
+            "Rosso",
+            "Seta",
+            "Zhu"
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lstRisultato.Items.Clear();
+            lstStudenti.Items.Clear();
+            lstVoti.Items.Clear();
+
+            if (string.IsNullOrEmpty(txtCinput.Text) || string.IsNullOrEmpty(txtNinput.Text))
+            {
+                MessageBox.Show("Devi inserire i valori n e c");
+            }
+            else
+            {
+                int n = Convert.ToInt32(txtNinput.Text);
+                int c = Convert.ToInt32(txtNinput.Text);
+
+                if (n > 20 || n < 0 || c < 0)
+                {
+                    MessageBox.Show("n non puo essere maggiore del numero di studenti massimo");
+                }
+                else
+                {
+                    for(int i = 0; i < n; i++)
+                    {
+                        lstStudenti.Items.Add(congomi[i]);
+                    }
+                    int[,] matriceVoti = new int[n, c];
+
+                    RiempiMatrice(matriceVoti);
+
+                    string[] arrayRis = TrasformaMatriceInArray(matriceVoti);
+
+                    lstVoti.Items.AddRange(arrayRis);
+
+
+                    for(int i = 0;i < n; i++)
+                    {
+                        double[] medieMaterie = CalcolaMedieMaterie(matriceVoti, i);
+                        string stringaOut = CostruisciStringaRisultato(matriceVoti, congomi[i], medieMaterie, i);
+
+                        lstRisultato.Items.Add(stringaOut);
+                    }
+                }
+            }
+
+        }
+
+        private string CostruisciStringaRisultato(int[,] matrice, string congome, double[] avgMaterie, int indexStd)
+        {
+            string output = "";
+
+            int nMateriaMax = TrovaIndiceMediaMax(avgMaterie);
+            double mediaMateriaMax = TrovaMediaMax(avgMaterie);
+            string votiPerMaxMateria = TrovaVotiPerMaxMateria(matrice, nMateriaMax);
+            string votoMineMax = TrovaVotoMineMax(matrice, indexStd);
+
+            output += $"Lo studente: {congome} ha la media più alta ({mediaMateriaMax}) nella materia: {nMateriaMax} la media è stata ottenuta con i voti {votiPerMaxMateria}, {votoMineMax}";
+
+
+            return output;
+        }
+
+        private string TrovaVotoMineMax(int[,] matrice, int indexStd)
+        {
+            string output = "";
+
+            int votoMin = int.MaxValue;
+            int votoMax = int.MinValue;
+
+            for (int i = 0; i < matrice.GetLength(1); i++)
+            {
+                if (matrice[indexStd, i] > votoMax)
+                {
+                    votoMax = matrice[indexStd, i];
+                }
+                if(matrice[indexStd, i] < votoMin)
+                {
+                    votoMin = matrice[indexStd, i];
+                }
+
+            }
+
+            return $"Voto Min: {votoMin}, Voto Max: {votoMax}";
+        }
+
+        private string TrovaVotiPerMaxMateria(int[,] matrice, int indexMax)
+        {
+            string output = "";
+
+            for(int i = 0; i < matrice.GetLength(1); i++)
+            {
+                output += matrice[indexMax, i] + " ";
+            }
+
+            return output;
+        }
+
+        private int TrovaIndiceMediaMax(double[] medieMaterie)
+        {
+            int output = int.MinValue;
+
+            for (int i = 0; i < medieMaterie.Length; i++)
+            {
+                if (medieMaterie[i] > output)
+                {
+                    output = i;
+                }
+            }
+
+            return output;
+        }
+
+        private double TrovaMediaMax(double[] medieMaterie)
+        {
+            double output = double.MinValue;
+
+            for(int i = 0; i < medieMaterie.Length; i++)
+            {
+                if (medieMaterie[i] > output)
+                {
+                    output = medieMaterie[i];
+                }
+            }
+
+            return output;
+        }
+
+        private double[] CalcolaMedieMaterie(int[,] matriceVoti, int nStudente)
+        {
+            double[] output = new double[matriceVoti.GetLength(0)];
+
+            for(int i = 0; i < matriceVoti.GetLength(0); i++)
+            {
+                for(int j = 0; j < matriceVoti.GetLength(1); j++)
+                {
+                    output[i] += matriceVoti[nStudente, j];
+                }
+                output[i] /= matriceVoti.GetLength(1);
+            }
+
+            return output;
+        }
+
+        private string[] TrasformaMatriceInArray(int[,] matriceVoti)
+        {
+            string[] output = new string[matriceVoti.GetLength(0)];
+
+            for (int i = 0; i < matriceVoti.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriceVoti.GetLength(1); j++)
+                {
+                    output[i] += Convert.ToString(matriceVoti[i, j]) + " ";
+                }
+            }
+            return output;
+        }
+
+        private void RiempiMatrice(int[,] matriceVoti)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < matriceVoti.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriceVoti.GetLength(1); j++)
+                {
+                    matriceVoti[i, j] = rnd.Next(1, 11);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
