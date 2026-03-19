d0f76d11dc4a5fe812a441f05bb2151119146323
Andrea Maffiotto <a.maffiotto.4046@vallauri.edu>
2026-03-19 09:59:09 +0100

    winforms

--- Informatica/[LAB] Maffiotto - WinForms 1/lesson/FrmMain.Designer.cs
+++ Informatica/[LAB] Maffiotto - WinForms 1/lesson/FrmMain.Designer.cs
@@ -77,117 +77,136 @@
             // 
             PcbGrid00.BackColor = Color.White;
             PcbGrid00.BorderStyle = BorderStyle.FixedSingle;
+            PcbGrid00.Cursor = Cursors.Hand;
             PcbGrid00.Enabled = false;
             PcbGrid00.Location = new Point(10, 7);
             PcbGrid00.Margin = new Padding(2);
             PcbGrid00.Name = "PcbGrid00";
             PcbGrid00.Size = new Size(106, 117);
+            PcbGrid00.SizeMode = PictureBoxSizeMode.StretchImage;
             PcbGrid00.TabIndex = 2;
             PcbGrid00.TabStop = false;
-            PcbGrid00.Click += FrmMain_Click;
+            PcbGrid00.Click += PcbCard_Click;
             // 
             // PcbGrid01
             // 
             PcbGrid01.BackColor = Color.White;
             PcbGrid01.BorderStyle = BorderStyle.FixedSingle;
+            PcbGrid01.Cursor = Cursors.Hand;
             PcbGrid01.Enabled = false;
             PcbGrid01.Location = new Point(120, 7);
             PcbGrid01.Margin = new Padding(2);
             PcbGrid01.Name = "PcbGrid01";
             PcbGrid01.Size = new Size(106, 117);
+            PcbGrid01.SizeMode = PictureBoxSizeMode.StretchImage;
             PcbGrid01.TabIndex = 3;
             PcbGrid01.TabStop = false;
-            PcbGrid01.Click += FrmMain_Click;
+            PcbGrid01.Click += PcbCard_Click;
             // 
             // PcbGrid02
             // 
             PcbGrid02.BackColor = Color.White;
             PcbGrid02.BorderStyle = BorderStyle.FixedSingle;
+            PcbGrid02.Cursor = Cursors.Hand;
             PcbGrid02.Enabled = false;
             PcbGrid02.Location = new Point(229, 7);
             PcbGrid02.Margin = new Padding(2);
             PcbGrid02.Name = "PcbGrid02";
             PcbGrid02.Size = new Size(106, 117);
+            PcbGrid02.SizeMode = PictureBoxSizeMode.StretchImage;
             PcbGrid02.TabIndex = 4;
             PcbGrid02.TabStop = false;
-            PcbGrid02.Click += FrmMain_Click;
+            PcbGrid02.Click += PcbCard_Click;
             // 
             // PcbGrid12
             // 
             PcbGrid12.BackColor = Color.White;
             PcbGrid12.BorderStyle = BorderStyle.FixedSingle;
+            PcbGrid12.Cursor = Cursors.Hand;
             PcbGrid12.Enabled = false;
             PcbGrid12.Location = new Point(229, 127);
             PcbGrid12.Margin = new Padding(2);
             PcbGrid12.Name = "PcbGrid12";
             PcbGrid12.Size = new Size(106, 117);
+            PcbGrid12.SizeMode = PictureBoxSizeMode.StretchImage;
             PcbGrid12.TabIndex = 7;
             PcbGrid12.TabStop = false;
-            PcbGrid12.Click += FrmMain_Click;
+            PcbGrid12.Click += PcbCard_Click;
             // 
             // PcbGrid11
             // 
             PcbGrid11.BackColor = Color.White;
             PcbGrid11.BorderStyle = BorderStyle.FixedSingle;
+            PcbGrid11.Cursor = Cursors.Hand;
             PcbGrid11.Enabled = false;
             PcbGrid11.Location = new Point(120, 127);
             PcbGrid11.Margin = new Padding(2);
             PcbGrid11.Name = "PcbGrid11";
             PcbGrid11.Size = new Size(106, 117);
+            PcbGrid11.SizeMode = PictureBoxSizeMode.StretchImage;
             PcbGrid11.TabIndex = 6;
             PcbGrid11.TabStop = false;
+            PcbGrid11.Click += PcbCard_Click;
             // 
             // PcbGrid10
             // 
             PcbGrid10.BackColor = Color.White;
             PcbGrid10.BorderStyle = BorderStyle.FixedSingle;
+            PcbGrid10.Cursor = Cursors.Hand;
             PcbGrid10.Enabled = false;
             PcbGrid10.Location = new Point(10, 127);
             PcbGrid10.Margin = new Padding(2);
             PcbGrid10.Name = "PcbGrid10";
             PcbGrid10.Size = new Size(106, 117);
+            PcbGrid10.SizeMode = PictureBoxSizeMode.StretchImage;
             PcbGrid10.TabIndex = 5;
             PcbGrid10.TabStop = false;
-            PcbGrid10.Click += FrmMain_Click;
+            PcbGrid10.Click += PcbCard_Click;
             // 
             // PcbGrid22
             // 
             PcbGrid22.BackColor = Color.White;
             PcbGrid22.BorderStyle = BorderStyle.FixedSingle;
+            PcbGrid22.Cursor = Cursors.Hand;
             PcbGrid22.Enabled = false;
             PcbGrid22.Location = new Point(229, 247);
             PcbGrid22.Margin = new Padding(2);
             PcbGrid22.Name = "PcbGrid22";
             PcbGrid22.Size = new Size(106, 117);
+            PcbGrid22.SizeMode = PictureBoxSizeMode.StretchImage;
             PcbGrid22.TabIndex = 10;
             PcbGrid22.TabStop = false;
-            PcbGrid22.Click += FrmMain_Click;
+            PcbGrid22.Click += PcbCard_Click;
             // 
             // PcbGrid21
             // 
             PcbGrid21.BackColor = Color.White;
             PcbGrid21.BorderStyle = BorderStyle.FixedSingle;
+            PcbGrid21.Cursor = Cursors.Hand;
             PcbGrid21.Enabled = false;
             PcbGrid21.Location = new Point(120, 247);
             PcbGrid21.Margin = new Padding(2);
             PcbGrid21.Name = "PcbGrid21";
             PcbGrid21.Size = new Size(106, 117);
+            PcbGrid21.SizeMode = PictureBoxSizeMode.StretchImage;
             PcbGrid21.TabIndex = 9;
             PcbGrid21.TabStop = false;
-            PcbGrid21.Click += FrmMain_Click;
+            PcbGrid21.Click += PcbCard_Click;
             // 
             // PcbGrid20
             // 
             PcbGrid20.BackColor = Color.White;
             PcbGrid20.BorderStyle = BorderStyle.FixedSingle;
+            PcbGrid20.Cursor = Cursors.Hand;
             PcbGrid20.Enabled = false;
             PcbGrid20.Location = new Point(10, 247);
             PcbGrid20.Margin = new Padding(2);
             PcbGrid20.Name = "PcbGrid20";
             PcbGrid20.Size = new Size(106, 117);
+            PcbGrid20.SizeMode = PictureBoxSizeMode.StretchImage;
             PcbGrid20.TabIndex = 8;
             PcbGrid20.TabStop = false;
-            PcbGrid20.Click += FrmMain_Click;
+            PcbGrid20.Click += PcbCard_Click;
             // 
             // FrmMain
             // 
@@ -207,8 +226,6 @@
             Controls.Add(BtnResetGame);
             Name = "FrmMain";
             Text = "Memory";
-            Load += FrmMain_Load;
-            Click += FrmMain_Click;
             ((System.ComponentModel.ISupportInitialize)PcbGrid00).EndInit();
             ((System.ComponentModel.ISupportInitialize)PcbGrid01).EndInit();
             ((System.ComponentModel.ISupportInitialize)PcbGrid02).EndInit();
--- Informatica/[LAB] Maffiotto - WinForms 1/lesson/FrmMain.cs
+++ Informatica/[LAB] Maffiotto - WinForms 1/lesson/FrmMain.cs
@@ -1,4 +1,4 @@
-using lesson;
+using Timer = System.Threading.Timer;
 
 namespace lesson
 {
@@ -15,6 +15,7 @@ namespace lesson
 
         // stato di gioco
         int remainingErrors;
+        int correctPairs;
         string[,] gameMatrix;
 
         PictureBox lastCard;
@@ -28,7 +29,9 @@ namespace lesson
 
         public void ResetGame()
         {
+            lastCard = null;
             remainingErrors = 3;
+            correctPairs = 0;
             GenerateGameGrid();
         }
 
@@ -56,39 +59,154 @@ namespace lesson
                     }
                     // ripeto finché è occupata
                     while (gameMatrix[x, y] != null);
+
+                    gameMatrix[x, y] = card;
                 }
             }
         }
 
         private void FlipCard(PictureBox flippedCard)
         {
+
+            int flippedX = flippedCard.Name[7] - '0'; // converto da carattere a intero
+            int flippedY = flippedCard.Name[8] - '0'; // togliendo '0' -> 48
+
+            // facciamo vedere la carta
+            flippedCard.Load($"assets/img/{gameMatrix[flippedX, flippedY]}");
+
+            // se è la bomba è auto-loss
+            if (gameMatrix[flippedX, flippedY] == bomb)
+            {
+                // perso
+                MessageBox.Show("Hai perso", "Memory", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                GameOver();
+
+                // tutte le picturebox del form (modo 1)
+                // calcola dal nome
+                for (int i = 0; i < GRID_SIZE; i++)
+                {
+                    for (int j = 0; j < GRID_SIZE; j++)
+                    {
+                        PictureBox pcbCard = (PictureBox)Controls[$"PcbGrid{i}{j}"];
+                        pcbCard.Enabled = false;
+                    }
+                }
+
+                // tutte le picturebox del form (modo 2) - foreach
+                // foreach (PictureBox pcb in Controls.OfType<PictureBox>())
+                // {
+                //    pcb.Enabled = false;
+                // }
+            }
+
             if (lastCard == null)
             {
                 lastCard = flippedCard;
+                lastCard.Enabled = false; // cosi non posso cliccarla
+                                          // due volte
             }
             else
             {
-                int flippedX = flippedCard.Name[7] - '0';
-                int flippedY = flippedCard.Name[8] - '0';
+                int lastX = lastCard.Name[7] - '0'; // converto da carattere a intero
+                int lastY = lastCard.Name[8] - '0'; // togliendo '0' -> 48
 
-                int lastX = flippedCard.Name[7] - '0';
-                int lastY = flippedCard.Name[8] - '0';
+                // do gia per scontato che flipped e last non siano nella stessa
+                // posizione
                 if (gameMatrix[flippedX, flippedY] == gameMatrix[lastX, lastY])
                 {
+                    // giusto, lo rendo permanente e vado avanti
+                    flippedCard.Enabled = false;
 
+                    correctPairs++;
+
+                    if (correctPairs == cards.Length)
+                    {
+                        // vinto, perchè ho fatto una coppia per ogni carta
+                        // quando ho fatto partire il gioco
+
+                        MessageBox.Show("Hai Vinto", "Memory", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        GameOver();
+                        // tutte le picturebox del form (modo 1)
+                        // calcola dal nome
+                        
+
+                        // tutte le picturebox del form (modo 2) - foreach
+                        // foreach (PictureBox pcb in Controls.OfType<PictureBox>())
+                        // {
+                        //    pcb.Enabled = false;
+                        // }
+                    }
                 }
-            }
+                else
+                {
+                    // sbagliato, perdo un tentativo e ricopro dopo un intervallo
+                    remainingErrors--;
 
+                    LblRemainingErrors.Text = $"Errori rimanenti: {remainingErrors}";
+
+                    if (remainingErrors == 0)
+                    {
+                        // perso
+                        MessageBox.Show("Hai perso", "Memory", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        // tutte le picturebox del form (modo 1)
+                        // calcola dal nome
+                        GameOver();
+
+                        // tutte le picturebox del form (modo 2) - foreach
+                        // foreach (PictureBox pcb in Controls.OfType<PictureBox>())
+                        // {
+                        //    pcb.Enabled = false;
+                        // }
+                    }
+                    else
+                    {
+                        // attesa ...
+                        Refresh();
+                        Thread.Sleep(2000);
+
+                        // ri-copro e riabilito tutto
+                        lastCard.Image = null;
+                        flippedCard.Image = null;
+
+                        lastCard.Enabled = true;
+                        flippedCard.Enabled = true;
+                    }
+                }
+
+                // in ogni caso la coppia pescata viene resettata
+                lastCard = null;
+            }
         }
 
+        private void GameOver()
+        {
+            BtnResetGame.Enabled = true;
+
+            for (int i = 0; i < GRID_SIZE; i++)
+            {
+                for (int j = 0; j < GRID_SIZE; j++)
+                {
+                    PictureBox pcbCard = (PictureBox)Controls[$"PcbGrid{i}{j}"];
+                    pcbCard.Enabled = false;
+                }
+            }
+        }
 
         // rendering
 
         private void BtnResetGame_Click(object sender, EventArgs e)
         {
-            lastCard = null;
             ResetGame();   // logica di gioco
             ResetGameUI(); // logica di rendering
+        }
+
+        private void PcbCard_Click(object sender, EventArgs e)
+        {
+            PictureBox flippedCard = (PictureBox)sender;
+            FlipCard(flippedCard);
+            FlipCardUI(flippedCard);
         }
 
         private void ResetGameUI()
@@ -112,19 +230,9 @@ namespace lesson
             LblRemainingErrors.Visible = true;
         }
 
-
-
-
-
-        // FUNZIONI PER SBAGLIO
-        private void FrmMain_Click(object sender, EventArgs e)
+        private void FlipCardUI(PictureBox flippedCard)
         {
-
-        }
-
-        private void FrmMain_Load(object sender, EventArgs e)
-        {
-
+            //throw new NotImplementedException();
         }
     }
 }
